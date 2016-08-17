fded540f8f26abf99e46b58d8caa1ee69e6eec61
olahfemi <olahfemi@gmail.com>
2016-08-17 14:44:12 +0100

    I think everything look alright, don't you think?

--- Evernote.js
+++ Evernote.js
@@ -30,7 +30,9 @@ class NotesApplication {
 	listNotes(){
 
 		for (let i = 0; i < this.notes.length; i++){
-			return ("ID:", i , this.notes[i], this.authorCaptured);
+			console.log("ID:", i );
+			console.log(this.notes[i]);
+			console.log("By Author", [this.authorCaptured]);
 		}
 	}
 	
@@ -52,12 +54,12 @@ class NotesApplication {
 	 * By Author [author]
 	 */
 	search(search_text){
-		for (x = 0; x < search_text.length; x++);
-		
-			for (i = 0; i < this.notes.length; i++){
-				if (search_text[x] === this.notes[i]){
-							
-						return "Yay!";
+		for (let i = 0; i < this.notes.length; i++){
+			if(this.notes.includes(search_text)){
+				console.log("Showing results for", search_text)
+				console.log("Note ID: ", i );
+				console.log(this.notes[i]);
+				console.log("By Author", [this.authorCaptured]);
 			}
 		}
 	}
@@ -66,9 +68,9 @@ class NotesApplication {
 	 * delete the note
 	 */
 	killer(note_id){
-		for (i = 0; i < notes.length; i++) { 
-			delete notes[i];
-			return (notes);
+		for (let i = 0; i < this.notes.length; i++) { 
+			delete this.notes[i];
+			return (this.notes);
 			}
 }
 	/**
@@ -76,11 +78,108 @@ class NotesApplication {
 	 */
 	modifyNote(note_id, new_content) {
 		
-		notes[note_id];
+		this.notes[note_id] = new_content;
+		return this.notes;
 	}
  }
  
  let n = new NotesApplication("Femi");
  n.create("Lorem");
+ n.create("Ipsum");
+ //n.killer(0);
+ //n.getNote(0);
+ // n.listNotes();
+ //n.search("Lorem");
+ n.modifyNote(1, "change")
+class NotesApplication {
+	 
+	 /**
+	  * constructor that takes author as a parameter
+	  * this constructor saves the author parameter as an instance variable
+	  * it has a notes list/array to store all the notes as an instance property
+	  */ 
+	constructor(author){
+		this.authorCaptured = author;
+		this.notes = [];
+		}
+	
+	/**
+	 * Take the note content as the parameter and 
+	 * add it to the notes list object
+	 */
+	create(note_content){
+		this.notes.push(note_content);
+		return this.notes;
+	}
+	
+	/**
+	 * the content of the notes list this way:
+	 * Note ID:[note_id]
+	 * [NOTE_CONTENT]
+	 *
+	 * By Author[author]
+	 */
+	listNotes(){
+
+		for (let i = 0; i < this.notes.length; i++){
+			console.log("ID:", i );
+			console.log(this.notes[i]);
+			console.log("By Author", [this.authorCaptured]);
+		}
+	}
+	
+	/**
+	 * note_id is the index of the note in the notes list
+	 * return the content of that note as a string
+	 */
+	getNote(note_id){
+		
+		return String(this.notes[note_id]);
+	}
+		
+	/**
+	 * This function take a search string, search_text
+	 * It returns all the notes with that text within it in the following format:
+	 * Showing results for search ‘[<search_text>]’
+	 * Note ID: [note_id]
+	 * [NOTE_CONTENT]
+	 * By Author [author]
+	 */
+	search(search_text){
+		for (let i = 0; i < this.notes.length; i++){
+			if(this.notes.includes(search_text)){
+				console.log("Showing results for", search_text)
+				console.log("Note ID: ", i );
+				console.log(this.notes[i]);
+				console.log("By Author", [this.authorCaptured]);
+			}
+		}
+	}
+	
+	/**
+	 * delete the note
+	 */
+	killer(note_id){
+		 
+		delete this.notes[note_id];
+		return (this.notes);
+			
+}
+	/**
+	 *  replace content in note at note_id with new_note
+	 */
+	modifyNote(note_id, new_content) {
+		
+		this.notes[note_id] = new_content;
+		return this.notes;
+	}
+ }
  
- n.listNotes()
+ let n = new NotesApplication("Femi");
+ n.create("Lorem");
+ n.create("Ipsum");
+ n.killer(2);
+ //n.getNote(0);
+ // n.listNotes();
+ //n.search("Lorem");
+ //n.modifyNote(1, "change")
